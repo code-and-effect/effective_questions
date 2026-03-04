f1eedc6059399d2d276b78894e6defdaf8f4ea10
Matt Riemer <matthew@codeandeffect.com>
2026-03-04 16:45:53 -0700

    Upgrade test suite

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,21 +1,24 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
+require 'sprockets/rails'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 require 'haml'
+require 'wicked'
 require 'devise'
-require 'effective_test_bot'
+require 'effective_datatables'
+
+require "effective_test_bot"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.load_defaults 8.1
+    config.autoload_lib(ignore: %w[assets tasks])
+    config.active_record.use_yaml_unsafe_load = true
+    config.active_job.queue_adapter = :inline
   end
 end
